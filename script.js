037d0ab0b3c308d23bc5cf865788b841aee3e060
venkatesan R <115686426+venkat-ram13@users.noreply.github.com>
2022-11-16 17:53:24 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+var txt = document.createElement("div");
+    txt.setAttribute("class","grid-container");
+        console.log(10);
+        txt.innerHTML = 10;
+        setTimeout(()=> {
+            console.log(9);
+            txt.innerHTML = 9;
+            setTimeout(()=> {
+                console.log(8);
+                txt.innerHTML = 8;
+                setTimeout(()=> {
+                    console.log(7);
+                    txt.innerHTML = 7;
+                    setTimeout(()=> {
+                        console.log(6);
+                        txt.innerHTML = 6;
+                        setTimeout(function() {
+                            console.log(5);
+                            txt.innerHTML = 5;
+                            setTimeout(function() {
+                                console.log(4);
+                                txt.innerHTML = 4;
+                                setTimeout(()=> {
+                                    console.log(3);
+                                    txt.innerHTML = 3;
+                                    setTimeout(()=> {
+                                        console.log(2);
+                                        txt.innerHTML = 2;
+                                        setTimeout(()=> {
+                                            console.log(1);
+                                            txt.innerHTML = 1;
+                                            setTimeout(()=> {
+                                                console.log("Happy Independence Day")
+                                                txt.innerHTML = `<b>Happy Independence Day</b>
+                                                 <img class="independence day"src="happy.jpg" alt="independence"></img>`
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+  document.body.append(txt);
